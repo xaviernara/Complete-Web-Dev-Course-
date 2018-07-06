5f6653964c82584ec77000a58c5e3465cf51dc86
Xavier Nara <32371616+xaviernara@users.noreply.github.com>
2018-07-06 00:41:00 -0700

    connected webpage to filezilla server
    learned how to use the filezilla server and how to use in conjuction with the ecowebhosting hosting

---
+++ 1-html/index.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html>
+
+	<head>
+									<title>Anime Is Life webpage</title>
+	</head>
+	
+	<body>
+
+		<h1 id="top"><em>ANIME IS LIFE</em></h1>
+		<p> <img src ="https://japanimetalks.files.wordpress.com/2015/09/otaku_wallpaper_1_by_legendaryrey-d8opa08.png" width ="1400" height="400"></p>
+		<hr>
+
+		
+		<p><strong><em>Favorite Anime</em></strong>
+			<ol>
+				<li> <img src = "https://a248.e.akamai.net/ib.huluim.com/show_key_art/2138?size=1600x600&region=US" width ="1000"> </li>
+				<br>
+				<p>One Piece (Japanese: ワンピース Hepburn: Wan Pīsu) is a Japanese manga series written and illustrated by Eiichiro Oda. <br>It has been serialized in Shueisha's Weekly Shōnen Jump magazine since July 22, 1997, and has been collected in 89 tankōbon volumes.<br> The story follows the adventures of Monkey D. Luffy, a boy whose body gained the properties of rubber after unintentionally eating a Devil Fruit. With his crew of pirates, named the Straw Hat Pirates, Luffy explores the Grand Line in search of the world's ultimate treasure known as "One Piece" in order to become the next Pirate King.</p>
+				<hr>
+
+				<li><img src="https://vignette.wikia.nocookie.net/vsbattles/images/5/50/Katekyo_Hitman_Reborn%21-LogoPNG.png/revision/latest?cb=20170616161425" width="1000"></li>
+				<p>Reborn!, known in Japan as Katekyō Hitman Reborn! (Japanese: 家庭教師ヒットマンリボーン! Hepburn: Katekyō Hittoman Ribōn!, Katekyō, a portmanteau of Katei Kyōshi, means "home tutor"), is a Japanese manga written and illustrated by Akira Amano. The manga is about a young boy, Tsunayoshi Sawada, who discovers that he is next in line to become boss of the Vongola family, 
+				   a powerful Mafia organization. The Vongolas' most powerful hitman, a gun-toting infant named Reborn, is sent to teach Tsuna how to be a boss</p>
+				<hr>
+
+				<li><img src="https://vignette.wikia.nocookie.net/naruto/images/4/48/Naruto_Shipp%C5%ABden_Logo.png/revision/latest?cb=20130404172829"></li>
+				<p>More than two years have passed since the most recent adventures in the Hidden Leaf Village, ample time for ninja wannabe Naruto Uzumaki to have developed skills worthy of recognition and respect. The hyperactive youngster has trained extensively with Jiraiya-sama and returned to Konoha to reunite with friends and, perhaps, partake in more pranks than schoolwork.<br> But Naruto is more determined than ever to become the greatest ninja, and his hard-earned improvements are put to the ultimate test against newer, stronger enemies.<br> "Naruto: Shippuden" is adapted from Part II of the "Naruto" comic book series by Masashi Kishimoto.</p>
+				<hr>
+
+				<li><img src="https://do09d4zv66caw.cloudfront.net/wp-content/uploads/2016/05/363f6c495f07bec06fd2ac10d2b8cc64.jpg" ></li>
+				<p>D.Gray-man (Japanese: ディー・グレイマン Hepburn: Dī Gureiman) is a Japanese manga series written and illustrated by Katsura Hoshino.<br> Set in the 19th century, it tells the story of a young Allen Walker, who joins an organization of exorcists named the Black Order. They use an ancient substance, Innocence, to combat a man known as the Millennium Earl and his demonic army of Akuma who intend to destroy humanity.<br> Many characters are adapted from Hoshino's previous works and drafts, such as Zone. The series is noted for its dark narrative; Hoshino once rewrote a scene she thought too violent for her young readers.</p>
+				<hr>
+				
+			<ol>
+		
+		</p><strong><sup>Favorite Fights<sup></strong>
+		<P>
+			<OL type="I">
+				<li><iframe width="560" height="315" src="https://www.youtube.com/embed/2MwGMHU2xKs" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></li>
+				<hr>
+				<li><iframe width="560" height="315" src="https://www.youtube.com/embed/unvOusFVaUM" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></li>
+				<hr>
+				<li><iframe width="560" height="315" src="https://www.youtube.com/embed/FvkExGuEak4" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></li>
+				<hr>
+				<li><iframe width="560" height="315" src="https://www.youtube.com/embed/B0zPieM0OUY" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe><li>
+			</ol>
+		</p>
+		<br>
+		
+		<p>
+			<form action ="https://www.masterani.me">
+			<input type="submit" value="Take the first step">
+			</form>
+		</p>
+
+		
+
+		<p><a href= "#top"> Back to Top</a></p>
+
+
+	</body>
+
+</html>
+
