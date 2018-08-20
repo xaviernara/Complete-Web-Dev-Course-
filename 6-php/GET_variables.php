bf015a83f0b4d3bf7ba24fc61b513021d5303535
Xavier Nara <32371616+xaviernara@users.noreply.github.com>
2018-08-19 20:04:53 -0700

    learned about GET variables in php
    learned how to GET user input from the url
    
    learned how to GET user input from the HTML input forms
    
    learned how to test if user input is numeric using GET variables
    
    learned how to test is user input is prime or not

---
+++ 6-php/GET_variables.php
@@ -0,0 +1,62 @@
+<?php
+//GET VARIABLES get the user input thats in the url and lets you manipulate them 
+
+//this determines if the user's numeric input is prime or not by using GET variables
+
+//the 1st if statement tests if the input is numeric, >0, when rounded has zero decimal points 
+   if(is_numeric($_GET['number']) && $_GET['number'] > 0 && $_GET['number'] == round($_GET['number'], 0)){
+        
+        $i = 2;
+        
+        $isPrime = true;
+        
+	//while the input's remainder is equal to 0 then it'll keep iterating 
+        while ($i < $_GET['number']) {
+            
+	    //if the input divided by 2's remainder is equal to 0 then it's not prime 
+            if ($_GET['number'] % $i == 0) {
+                
+                // Number is not prime!
+                
+                $isPrime = false;
+                
+            }
+
+            $i++;
+            
+        }
+        
+	//if its prime then the number will be displayed on the screen
+	//else a its not prime message will be displayed
+        if ($isPrime) {
+            
+            echo "<p>".$i." is a prime number!</p>";
+            
+        } else {
+            
+            echo "<p>".$i." is not prime.</p>";
+            
+        }  
+        
+    } else if ($_GET) {
+        
+        // User has submitted something which is not a positive whole number
+	//(ie a string)
+        
+        echo "<p>Please enter a positive whole number.</p>";
+        
+    }
+
+?>
+
+//HTML IS WRITTEN OUTSIDE THE PHP 
+//this is html is to get numeric input from the user and using the GET variables in php you can manipulate the input 
+<p>Please enter a whole number.</p>
+
+<form>
+
+    <input name="number" type="text">
+    
+    <input type="submit" value="Go!">
+    
+</form>
